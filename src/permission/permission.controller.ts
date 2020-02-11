e621054fd7ccf0c909251627da82ab8b8fa4cc7e
kyuuube <benchiu@yandex.com>
2020-02-11 09:51:55 +0800

    权限功能

--- src/permission/permission.controller.ts
+++ src/permission/permission.controller.ts
@@ -14,6 +14,7 @@ import { PermissionService } from './permission.service'
 import { ApiTags, ApiBearerAuth, ApiOperation } from '@nestjs/swagger'
 import { PermissionDto } from './permission.dto'
 import { AuthGuard } from '@nestjs/passport'
+import { PaginationDto } from '../common/dto/pagination.dto'
 
 @Controller('permission')
 @ApiTags('权限')
@@ -22,10 +23,43 @@ export class PermissionController {
     constructor(private readonly permissionService: PermissionService) {}
 
     @Post('/create')
-    @ApiOperation({ summary: '创建菜单' })
+    @ApiOperation({ summary: '创建权限' })
     @ApiBearerAuth()
     @UseGuards(AuthGuard('jwt'))
     public createMenu(@Body() dto: PermissionDto) {
         return this.permissionService.create(dto)
+    }
+
+
+    @Put('/edit')
+    @ApiOperation({ summary: '修改权限' })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    public editPerm(@Body() dto: PermissionDto) {
+        return this.permissionService.edit(dto)
+    }
+
+    @Delete('/delete/:id')
+    @ApiOperation({ summary: '删除权限' })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    public deletePerm(@Param('id') id: string) {
+        return this.permissionService.delete(id)
+    }
+
+    @Get('/list')
+    @ApiOperation({ summary: '获取权限列表' })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    public getPermList(@Query() params: PaginationDto) {
+        return this.permissionService.getList(params)
+    }
+
+    @Get('/detail/:id')
+    @ApiOperation({ summary: '获取权限详情' })
+    @ApiBearerAuth()
+    @UseGuards(AuthGuard('jwt'))
+    public getPermDetail(@Param('id') id: string) {
+        return this.permissionService.detail(id)
     }
 }
--- src/permission/permission.service.ts
+++ src/permission/permission.service.ts
@@ -2,6 +2,7 @@ import { HttpException, Injectable, Logger, HttpStatus } from '@nestjs/common'
 import { Transport } from '@nestjs/common/enums/transport.enum'
 import { Client, ClientProxy } from '@nestjs/microservices'
 import { PermissionDto } from './permission.dto'
+import { PaginationDto } from '../common/dto/pagination.dto'
 
 @Injectable()
 export class PermissionService {
@@ -16,6 +17,42 @@ export class PermissionService {
     public async create(data: PermissionDto) {
         return this.client
             .send({ cmd: 'create perm' }, data)
+            .toPromise()
+            .catch(error => {
+                throw new HttpException(error.message, HttpStatus.FORBIDDEN)
+            })
+    }
+
+    public async edit(data: PermissionDto) {
+        return this.client
+            .send({ cmd: 'edit perm' }, data)
+            .toPromise()
+            .catch(error => {
+                throw new HttpException(error.message, HttpStatus.FORBIDDEN)
+            })
+    }
+
+    public async delete(id: string) {
+        return this.client
+            .send({ cmd: 'del perm' }, id)
+            .toPromise()
+            .catch(error => {
+                throw new HttpException(error.message, HttpStatus.FORBIDDEN)
+            })
+    }
+
+    public async detail(id: string) {
+        return this.client
+            .send({ cmd: 'perm detail' }, id)
+            .toPromise()
+            .catch(error => {
+                throw new HttpException(error.message, HttpStatus.FORBIDDEN)
+            })
+    }
+
+    public async getList(data: PaginationDto) {
+        return this.client
+            .send({ cmd: 'perm list' }, data)
             .toPromise()
             .catch(error => {
                 throw new HttpException(error.message, HttpStatus.FORBIDDEN)
